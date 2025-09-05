053e0d37a0e34e737ce6e5f2d32b8a370e9e81e4
Petar Karadzhov <p.karadzhovv@gmail.com>
2025-09-05 10:01:50 +0300

    Uploading Azure Bastion files

---
+++ terraform-bastion/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # version = "4.23.0" # ommiting will result to latest
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
